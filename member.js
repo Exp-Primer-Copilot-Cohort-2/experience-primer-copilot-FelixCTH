d839311970c8492f539d238f7d8ff3322390ba59
FelixCTH <159406734+FelixCTH@users.noreply.github.com>
2024-07-22 06:29:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "John";
+    member.age = 25;
+    member.skills = ["C#", "ASP.NET", "MVC", "jQuery"];
+    return member;
+}
